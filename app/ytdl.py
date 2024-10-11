eff671de769ed4018a26ffbf60322cbe6da40621
nichuanfang <f18326186224@gmail.com>
2024-10-11 10:04:50 +0800

    feat: add update metube scripts

--- app/ytdl.py
+++ app/ytdl.py
@@ -64,6 +64,25 @@ class Download:
         self.loop = None
         self.notifier = None
 
+    def _replace_special_chars(file_name):
+        replacements = {
+            '"': '＂',
+            "'": '＇',
+            ' ': '　',
+            ':': '：',
+            '/': '／',
+            '\\': '＼',
+            '*': '＊',
+            '?': '？',
+            '>': '＞',
+            '<': '＜',
+            '|': '｜'
+        }
+
+        for old_char, new_char in replacements.items():
+            file_name = file_name.replace(old_char, new_char)
+
+        return file_name
 
     def _download(self):
         try:
@@ -104,7 +123,7 @@ class Download:
             # 如果是m4a格式的 需要使用ffmpeg将flac转为aac格式
             if self.info.format == "m4a":
                 # 获取下载的文件路径
-                downloaded_file = os.path.join(self.download_dir, f'{self.info.title}.flac')
+                downloaded_file = os.path.join(self.download_dir, f'{self._replace_special_chars(self.info.title)}.flac')
                 output_file = os.path.join(self.download_dir, self.info.title + '.m4a')
                 if self.info.quality == 'best':
                     bitrate = '320'
